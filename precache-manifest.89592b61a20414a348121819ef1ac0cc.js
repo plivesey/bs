d436f5ca4bd74be8dfb4f5a5cf403912a5c84987
Peter Livesey <plivesey453@gmail.com>
2018-11-03 15:13:03 +0700

    Updates

---
+++ precache-manifest.89592b61a20414a348121819ef1ac0cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e4bbd691bedeb34b096",
+    "url": "/bs-web/static/js/runtime~main.5e4bbd69.js"
+  },
+  {
+    "revision": "c28c6a3d85b8fabf6c59",
+    "url": "/bs-web/static/js/main.c28c6a3d.chunk.js"
+  },
+  {
+    "revision": "af872d2da0eca0aa6db2",
+    "url": "/bs-web/static/js/1.af872d2d.chunk.js"
+  },
+  {
+    "revision": "c28c6a3d85b8fabf6c59",
+    "url": "/bs-web/static/css/main.d3a0cd99.chunk.css"
+  },
+  {
+    "revision": "75074a932c204ed4be5d2df3eaeb6640",
+    "url": "/bs-web/index.html"
+  }
+];
